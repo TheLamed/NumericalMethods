ae1f6900e281a48131324302bd475ec4576418b8
Marko Kvyk <markomar2000@gmail.com>
2020-06-04 21:45:41 +0300

    LU

---
+++ NumericalMethods/NumericalMethods/LU.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumericalMethods
+{
+    public class LU
+    {
+        public double[,] LUFactorization(double[,] matrix, out double[,] C)
+        {
+            double[,] B = new double[matrix.GetLength(0), matrix.GetLength(1)];
+            C = new double[matrix.GetLength(0), matrix.GetLength(1)];
+            for (int i = 0, j = 2; i < matrix.GetLength(0); i++, j--)
+            {
+                for (int l = 0; l < i + 1; l++)
+                {
+                    B[l, 0] = matrix[l, 0];
+                    if (i > 0)
+                    {
+                        double sum = 0;
+                        for (int k = 0; k < l; k++)
+                        {
+                            sum = B[i, k] * C[k, l] + sum;
+                        }
+                        B[i, l] = matrix[i, l] - sum;
+                        sum = 0;
+                    }
+                }
+                for (int l = 0; l < j + 1; l++)
+                {
+                    C[0, l] = matrix[0, l] / B[0, 0];
+                    if (j < 2)
+                    {
+                        for (int m = 2; m > i - 1; m--)
+                        {
+                            double sum = 0;
+                            for (int k = 0; k < i; k++)
+                            {
+                                sum = B[i, k] * C[k, m] + sum;
+                            }
+                            C[i, m] = (1 / B[i, i]) * (matrix[i, m] - sum);
+                            sum = 0;
+                        }
+                    }
+                }
+            }
+            return B;
+        }
+    }
+}
--- NumericalMethods/NumericalMethods/Program.cs
+++ NumericalMethods/NumericalMethods/Program.cs
@@ -10,6 +10,7 @@ namespace NumericalMethods
     {
         static void Main(string[] args)
         {
+            //KRAMER
             //var kramer = new Kramer();
 
             //var matrix = new double[,]
@@ -21,25 +22,56 @@ namespace NumericalMethods
             //var list = kramer.Calculate(matrix);
             //Console.WriteLine("Розв'язки: " + string.Join(", ", list));
 
-            GausMethod Solution = new GausMethod(3, 3);
+            //GAUSS
+            //GausMethod Solution = new GausMethod(3, 3);
 
-            var matrix = new double[,]
-            {
-                { 2, 5,  4, 30  },
-                { 1, 3,  2, 150 },
-                { 2, 10, 9, 110 },
+            //var matrix = new double[,]
+            //{
+            //    { 2, 5,  4, 30  },
+            //    { 1, 3,  2, 150 },
+            //    { 2, 10, 9, 110 },
+            //};
+
+            //Solution.RightPart[0] = matrix[0, 3];
+            //Solution.RightPart[1] = matrix[1, 3];
+            //Solution.RightPart[2] = matrix[2, 3];
+            //for (int i = 0; i < 3; i++)
+            //    for (int j = 0; j < 3; j++)
+            //        Solution.Matrix[i][j] = matrix[i, j];
+
+            //Solution.SolveMatrix();
+
+            //Console.WriteLine(Solution.Answer[0] + ", " + Solution.Answer[1] + ", " + Solution.Answer[2]);
+
+            //LU
+            var LU = new LU();
+
+            double[,] Arr = 
+            { 
+                {2, 5,  4},
+                {1, 3,  2},
+                {2, 10, 9}
             };
 
-            Solution.RightPart[0] = matrix[0, 3];
-            Solution.RightPart[1] = matrix[1, 3];
-            Solution.RightPart[2] = matrix[2, 3];
-            for (int i = 0; i < 3; i++)
-                for (int j = 0; j < 3; j++)
-                    Solution.Matrix[i][j] = matrix[i, j];
+            double[,] C1;
+            Arr = LU.LUFactorization(Arr, out C1);
+            Show(Arr);
+            Console.WriteLine();
+            Show(C1);
+            Console.ReadKey();
+        }
 
-            Solution.SolveMatrix();
-
-            Console.WriteLine(Solution.Answer[0] + ", " + Solution.Answer[1] + ", " + Solution.Answer[2]);
+        private static void Show(double[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write("\t");
+                    Console.Write(matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
